9d6ddd74af5d1ecf9c2b73bf0180e7fc8c394c08
Samuel Caro <a20170086@pucp.edu.pe>
2020-05-04 18:21:44 -0500

    Base lab3

---
+++ src/main/java/com/example/lab3sw2/Lab3sw2Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3sw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab3sw2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab3sw2Application.class, args);
+    }
+
+}
